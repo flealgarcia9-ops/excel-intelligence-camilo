2e029510bb4f901311c440518ca0f030a218ff79
fabian leal <flealgarcia9@gmail.com>
2026-05-19 05:11:39 -0500

    Fix: rutas relativas para produccion (Hostinger/GitHub Pages)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/excel-intelligence-camilo/',
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: true,
